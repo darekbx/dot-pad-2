db0c3e949d862c6f5eb7d102f1079cf818d81ee3
darek <darek@cosmose.co>
2019-02-25 13:00:13 +0100

    Redesign

--- app/src/main/java/com/dotpad2/ui/dots/DotsActivity.kt
+++ app/src/main/java/com/dotpad2/ui/dots/DotsActivity.kt
@@ -1,35 +1,19 @@
 package com.dotpad2.ui.dots
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_dots.*
 import com.dotpad2.R
 
 class DotsActivity : AppCompatActivity() {
 
-    private val navigationListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        /*when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }*/
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dots)
+    }
 
-        navigation.setOnNavigationItemSelectedListener(navigationListener)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_dots, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
